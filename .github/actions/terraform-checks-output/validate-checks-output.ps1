d55a110ad6d23a1068ac0354fa828cdf77b34352
elmerfds <e.fds@outlook.com>
2024-10-09 22:07:27 +0100

    chore: Add YAML linting configuration and Terraform actions

---
+++ .github/actions/terraform-checks-output/validate-checks-output.ps1
@@ -0,0 +1,24 @@
+﻿param (
+    [Parameter(Mandatory = $true)]
+    [string]$stateFilePath
+)
+
+$state = Get-Content $stateFilePath | ConvertFrom-Json
+$checks = $state.check_results
+$checks_failed = $false
+
+foreach ($check in $checks) {
+    if ($($check.status) -eq "pass") {
+        Write-Output "$($check.config_addr) ✔"
+    }
+    else {
+        $checks_failed = $true
+        Write-Output "$($check.config_addr) ❌"
+        foreach ($msg in $($check.objects[0].failure_messages)) {
+            Write-Output "  - $($msg)"
+        }
+    }
+}
+if ($checks_failed) {
+    exit 1
+}
